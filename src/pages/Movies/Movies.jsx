af1ad17382ebf5a8654e95767fb62edc9a0e49c8
Kateryna Ostrynska <kateriinag@gmail.com>
2022-12-21 15:37:17 +0200

    Create Movies.jsx

---
+++ src/pages/Movies/Movies.jsx
@@ -0,0 +1,11 @@
+// import { useState, useEffect } from 'react';
+// import * as API from '../../services/api';
+// import { Link, Outlet } from 'react-router-dom';
+
+export const Movies = () => {
+  return (
+    <main>
+      <h1>Movies</h1>
+    </main>
+  );
+};
