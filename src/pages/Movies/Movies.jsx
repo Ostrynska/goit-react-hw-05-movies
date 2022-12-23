209cae707f91cb06e45d9eb5b6e8fb8f5daea956
Kateryna Ostrynska <kateriinag@gmail.com>
2022-12-23 14:47:54 +0200

    Update Movies.jsx

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,13 +1,9 @@
 // import { useState, useEffect } from 'react';
 import * as API from '../../services/api';
-import { MovieDetails } from '../../components/MovieDetails/MovieDetails';
+
 // import { Link, Outlet } from 'react-router-dom';
 
 export const Movies = () => {
-  const movies = API.getDetails();
-  return (
-    <main>
-      <MovieDetails movies={movies} />
-    </main>
-  );
+  // const movies = API.getDetails();
+  return <main></main>;
 };
