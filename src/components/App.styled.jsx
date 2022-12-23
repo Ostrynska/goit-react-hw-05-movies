04c03d88c98bd5ec8fc88e4d6be1a741f5185ac1
Kateryna Ostrynska <kateriinag@gmail.com>
2022-12-23 13:55:16 +0200

    Update App.styled.jsx

--- src/components/App.styled.jsx
+++ src/components/App.styled.jsx
@@ -2,9 +2,9 @@ import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 
 export const Container = styled.div`
-  max-width: 960px;
+  max-width: 100%;
   margin: 0 auto;
-  padding: 0 16px;
+  padding: 0 40px;
 `;
 
 export const Header = styled.header`
