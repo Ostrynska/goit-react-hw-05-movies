4f8ca3132081a424412720010cd6b91ec30c394c
Kateryna Ostrynska <kateriinag@gmail.com>
2022-12-23 22:34:32 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,7 +16,6 @@ export const App = () => {
               Home
             </Link>
             <Link to="/movies">Movies</Link>
-            {/* <Link to="/products">Products</Link> */}
           </nav>
           <Logo>
             <span role="img" aria-label="movie icon">
