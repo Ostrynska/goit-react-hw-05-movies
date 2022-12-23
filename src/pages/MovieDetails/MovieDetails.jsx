fde7ac3aa0a4b2f73618d066403cb093d13b0504
Kateryna Ostrynska <kateriinag@gmail.com>
2022-12-23 23:59:17 +0200

    Update MovieDetails.jsx

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import * as API from '../../services/api';
 
 export const MovieDetails = () => {
@@ -23,9 +24,6 @@ export const MovieDetails = () => {
 
   const { title, poster_path, release_date, popularity, overview, genres } =
     details;
-  const genreName = genres.map(genres => genres.name);
-
-  console.log(details);
 
   return (
     <main>
@@ -42,9 +40,17 @@ export const MovieDetails = () => {
       <h4>Overview</h4>
       <p>{overview}</p>
       <h4>Genres</h4>
-      <p>{genreName.toString()}</p>
-      {/* <p>{genres.map(genre => genre.name)}</p> */}
-      <p>Additional Information</p>
+      <p>{genres && genres.map(genre => genre.name).join(', ')}</p>
+      <h4>Additional Information</h4>
+      <ul>
+        <li>
+          <Link to="cast">Cast</Link>
+        </li>
+        <li>
+          <Link to="reviews">Reviews</Link>
+        </li>
+      </ul>
+      <Outlet />
     </main>
   );
 };
