6092ec9d2a6aacde3b6d3695b47ecaa06f5ab6f3
Kateryna Ostrynska <kateriinag@gmail.com>
2022-12-23 22:34:46 +0200

    Update MovieDetails.jsx

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,29 +1,50 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { MovieInformation } from '../../components/MovieInformation/MovieInformation';
 import * as API from '../../services/api';
 
 export const MovieDetails = () => {
-  const [movie, setMovie] = useState(null);
+  const [details, setDetails] = useState([]);
   const { id } = useParams();
 
   useEffect(() => {
     renderMovieDetails();
   }, []);
-  // console.log(movie);
 
   const renderMovieDetails = async () => {
     try {
-      const data = await API.getDetails(id);
-      // console.log('data', data);
-      setMovie(data);
+      const results = await API.getDetails(id);
+      setDetails(results);
     } catch (error) {
       console.log(error);
     }
   };
+
+  if (!details) return;
+
+  const { title, poster_path, release_date, popularity, overview, genres } =
+    details;
+  const genreName = genres.map(genres => genres.name);
+
+  console.log(details);
+
   return (
     <main>
-      <MovieInformation movie={movie} />
+      <button type="button">Go back</button>
+      <h3>
+        {title} ({release_date})
+      </h3>
+
+      <img src={'https://image.tmdb.org/t/p/w500' + poster_path} alt={title} />
+      <h3>
+        {title} ({release_date})
+      </h3>
+      <p>User Score: {popularity}</p>
+      <h4>Overview</h4>
+      <p>{overview}</p>
+      <h4>Genres</h4>
+      <p>{genreName.toString()}</p>
+      {/* <p>{genres.map(genre => genre.name)}</p> */}
+      <p>Additional Information</p>
     </main>
   );
 };
