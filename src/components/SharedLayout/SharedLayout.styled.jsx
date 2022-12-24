30bc46693d6708c5a4f1b72b0fbcf9e2384e768b
Kateryna Ostrynska <kateriinag@gmail.com>
2022-12-24 15:15:13 +0200

    Update SharedLayout.styled.jsx

--- src/components/SharedLayout/SharedLayout.styled.jsx
+++ src/components/SharedLayout/SharedLayout.styled.jsx
@@ -2,9 +2,9 @@ import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 
 export const Container = styled.div`
-  max-width: 960px;
+  max-width: 100%;
   margin: 0 auto;
-  padding: 0 16px;
+  padding: 0 40px;
 `;
 
 export const Header = styled.header`
@@ -35,6 +35,6 @@ export const Link = styled(NavLink)`
 
   &.active {
     color: white;
-    background-color: orangered;
+    background-color: #301934;
   }
 `;
