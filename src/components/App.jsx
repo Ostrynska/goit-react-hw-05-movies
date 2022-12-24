2aa1bdaf5663269c84676879887f78b5655749d2
Kateryna Ostrynska <kateriinag@gmail.com>
2022-12-24 15:15:29 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
-import { Container, Logo, Header, Link } from './App.styled';
+
+import { SharedLayout } from '../components/SharedLayout/SharedLayout';
 import { Home } from '../pages/Home/Home';
 import { Movies } from '../pages/Movies/Movies';
 import { MovieDetails } from '../pages/MovieDetails/MovieDetails';
@@ -9,32 +10,17 @@ import { NotFound } from '../components/NotFound';
 
 export const App = () => {
   return (
-    <>
-      <Container>
-        <Header>
-          <nav>
-            <Link to="/" end>
-              Home
-            </Link>
-            <Link to="/movies">Movies</Link>
-          </nav>
-          <Logo>
-            <span role="img" aria-label="movie icon">
-              🎬
-            </span>{' '}
-            Movie Room
-          </Logo>
-        </Header>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="movies" element={<Movies />} />
-          <Route path="movies/:id" element={<MovieDetails />}>
-            <Route path="cast" element={<Cast />} />
-            <Route path="reviews" element={<Reviews />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Container>
-    </>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="movies" element={<Movies />} />
+
+        <Route path="movies/:id" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 };
