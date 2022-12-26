3b8068351a5600c4a96135906c14d2a1c26ae135
Kateryna Ostrynska <kateriinag@gmail.com>
2022-12-26 20:51:41 +0200

    Update MovieDetails.styled.jsx

--- src/pages/MovieDetails/MovieDetails.styled.jsx
+++ src/pages/MovieDetails/MovieDetails.styled.jsx
@@ -81,3 +81,24 @@ export const TextScore = styled.span`
   margin-left: 10px;
   font-weight: 700;
 `;
+
+export const AdditionalList = styled.ul`
+  display: flex;
+`;
+
+export const AdditionalItem = styled.li`
+  width: 80px;
+  text-align: center;
+  border: 1px solid #301934;
+  padding: 0.3rem;
+  border-radius: 10px;
+  transition: background-color 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  :last-child {
+    margin-left: 1rem;
+  }
+  :hover {
+    background-color: #e6e6fa;
+    box-shadow: 0px 3px 1px rgb(0 0 0 / 10%), 0px 1px 2px rgb(0 0 0 / 8%),
+      0px 2px 2px rgb(0 0 0 / 12%);
+  }
+`;
