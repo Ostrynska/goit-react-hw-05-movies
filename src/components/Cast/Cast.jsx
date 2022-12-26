c011f6210a05db5cb9ff976fab18b27328a8d4bd
Kateryna Ostrynska <kateriinag@gmail.com>
2022-12-26 15:28:44 +0200

    Update Cast.jsx

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,10 +1,22 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import {
+  CastSection,
+  CastListWrapp,
+  CastListItem,
+  CastImageWrapp,
+  CastImage,
+  TextWrapp,
+  Text,
+  TextAccent,
+} from '../Cast/Cast.styled';
+import defaultCastImage from '../../images/cast.jpg';
 import * as API from '../../services/api';
 
 export const Cast = () => {
   const [cast, setCast] = useState([]);
   const { id } = useParams();
+  const baseURL = 'https://image.tmdb.org/t/p/w200';
 
   useEffect(() => {
     renderMovieCast();
@@ -22,22 +34,30 @@ export const Cast = () => {
   if (!cast || cast.length === 0) return;
 
   return (
-    <section>
-      <ul>
+    <CastSection>
+      <CastListWrapp>
         {cast &&
           cast.map(({ profile_path, name, character, id }) => {
             return (
-              <li key={{ id }}>
-                <img
-                  src={'https://image.tmdb.org/t/p/w200' + profile_path}
-                  alt={name}
-                />
-                <p>Name: {name}</p>
-                <p>Character: {character}</p>
-              </li>
+              <CastListItem key={{ id }}>
+                <CastImageWrapp>
+                  <CastImage
+                    src={
+                      profile_path
+                        ? `${baseURL}${profile_path}`
+                        : defaultCastImage
+                    }
+                    alt={name}
+                  />
+                </CastImageWrapp>
+                <TextWrapp>
+                  <TextAccent>{name}</TextAccent>
+                  <Text>Character: {character}</Text>
+                </TextWrapp>
+              </CastListItem>
             );
           })}
-      </ul>
-    </section>
+      </CastListWrapp>
+    </CastSection>
   );
 };
