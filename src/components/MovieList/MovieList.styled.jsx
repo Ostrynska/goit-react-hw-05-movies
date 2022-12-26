5cd76bf80f6d656f5a50b86251946ce9a0b4b0db
Kateryna Ostrynska <kateriinag@gmail.com>
2022-12-26 15:28:56 +0200

    Update MovieList.styled.jsx

--- src/components/MovieList/MovieList.styled.jsx
+++ src/components/MovieList/MovieList.styled.jsx
@@ -1,13 +1,36 @@
 import styled from 'styled-components';
 
 export const MovieListWrapp = styled.ul`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-  grid-column-gap: 35px;
-  grid-row-gap: 15px;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 33px;
+  align-items: stretch;
+  justify-content: start;
 `;
 
 export const MovieListItem = styled.li`
-  border: 1px solid black;
-  border-radius: 10px;
+  max-width: 200px;
+  transition: transform 0.15s ease-in-out;
+  :hover {
+    transform: scale(1.05);
+    color: #301934;
+  }
+`;
+
+export const MovieTitle = styled.h2`
+  text-transform: uppercase;
+  font-size: 14px;
+  color: black;
+  :hover {
+    color: #301934;
+  }
+`;
+
+export const MoviePoster = styled.img`
+  width: 100%;
+  height: auto;
+  border: 1px solid #797979;
+  border-radius: 5px;
+  margin-bottom: 5px;
+  display: block;
 `;
