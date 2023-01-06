3625fc332838bdd9413695db7f71d63d8a26e6b7
Kateryna Ostrynska <kateriinag@gmail.com>
2023-01-06 12:42:43 +0200

    Update MovieList.jsx

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 import {
   MovieListWrapp,
   MovieListItem,
@@ -8,12 +8,13 @@ import {
 import defaultPosterImage from '../../images/poster.jpg';
 
 export const MovieList = ({ movies }) => {
+  const location = useLocation();
   const baseURL = 'https://image.tmdb.org/t/p/w200';
   return (
     <MovieListWrapp>
       {movies.map(({ title, id, poster_path }) => (
         <MovieListItem key={id}>
-          <Link to={`movies/${id}`}>
+          <Link to={`movies/${id}`} state={{ from: location }}>
             <MoviePoster
               src={
                 poster_path ? `${baseURL}${poster_path}` : defaultPosterImage
