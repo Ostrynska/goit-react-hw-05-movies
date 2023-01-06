558404c5ab74ae0082cbad3077f7ec145e98d8f7
Kateryna Ostrynska <kateriinag@gmail.com>
2023-01-06 16:32:53 +0200

    Update Reviews.jsx

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
 import * as API from '../../services/api';
+
 import {
   ReviewsSection,
   ReviewsListWrapp,
@@ -34,16 +37,25 @@ export const Reviews = () => {
   return (
     <ReviewsSection>
       <ReviewsListWrapp>
-        {reviews &&
-          reviews.map(({ author, content, id }) => {
-            return (
-              <ReviewsListItem key={{ id }}>
-                <ReviewsAuthor>Author: {author}</ReviewsAuthor>
-                <Text>{content}</Text>
-              </ReviewsListItem>
-            );
-          })}
+        {reviews.map(({ author, content, id }) => {
+          return (
+            <ReviewsListItem key={{ id }}>
+              <ReviewsAuthor>Author: {author}</ReviewsAuthor>
+              <Text>{content}</Text>
+            </ReviewsListItem>
+          );
+        })}
       </ReviewsListWrapp>
     </ReviewsSection>
   );
+};
+
+Reviews.propTypes = {
+  reviews: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      author: PropTypes.string.isRequired,
+      content: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
