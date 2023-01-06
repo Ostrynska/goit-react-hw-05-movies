e2dce7b3c71715900eb7c64ab5fdd3bb2d50eea1
Kateryna Ostrynska <kateriinag@gmail.com>
2023-01-06 12:43:15 +0200

    Update Movies.jsx

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,24 +1,53 @@
+import { useState, useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
-import { SearchBox } from '../components/SearchBox';
+import { SearchBox } from '../../components/SearchBox/SearchBox';
+import { Main, MoviesSection } from './Movie.styled';
+import { MovieList } from '../../components/MovieList/MovieList';
 import * as API from '../../services/api';
 
 export const Movies = () => {
-  const movies = API.getSearch();
+  const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState('');
   const [searchParams, setSearchParams] = useSearchParams();
-  const movieName = searchParams.get('name') ?? '';
 
-  const visibleMovies = movies.filter(movie =>
-    movie.name.toLowerCase().includes(movieName.toLowerCase())
-  );
+  const movieSearch = searchParams.get('query');
 
-  const updateQueryString = name => {
-    const nextParams = name !== '' ? { name } : {};
-    setSearchParams(nextParams);
+  const handleChange = e => {
+    setQuery(e.currentTarget.value.toLowerCase());
   };
 
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    setSearchParams({ query: query });
+    setQuery('');
+  };
+
+  useEffect(() => {
+    if (!movieSearch) return;
+
+    async function renderMovie() {
+      try {
+        const { results } = await API.getSearch(movieSearch);
+        setMovies(results);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    renderMovie();
+  }, [movieSearch]);
+
+  console.log(movies);
   return (
-    <main>
-      <SearchBox value={movieName} onChange={updateQueryString} />
-    </main>
+    <Main>
+      <MoviesSection>
+        <SearchBox
+          value={query}
+          onChange={handleChange}
+          onSubmit={handleSubmit}
+        />
+        {movies && <MovieList movies={movies} />}
+      </MoviesSection>
+    </Main>
   );
 };
